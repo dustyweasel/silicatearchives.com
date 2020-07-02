f69d9d274cc06c61865e783b9f91fd78ed4a4fae
dustyweasel <dustyweasel@protonmail.com>
2020-07-02 02:23:12 -0500

    not a real project

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+    //write code in here that you want to execute
+
+        System.out.println("Hello world!");
+    }
+
+}
